932ec3cd95bd4e737aa10c8de959ed9ca223c993
Meghana <meghana@thinkful.commgit config --global user.email meghana@thinkful.com>
2019-01-05 13:38:07 -0500

    worked on GIT API

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+$(".getGitHubRepo").submit(function (e) {
+  e.preventDefault();
+  console.log("clicked");
+  let gitHandle = $('#repoInput').val();
+  console.log(gitHandle);
+  getUserRepo(gitHandle);
+});
+
+function getUserRepo(gitHandle) {
+  fetch(`https://api.github.com/users/${gitHandle}/repos`)
+    .then(response => response.json())
+    .then(responseJson => getRepoNameAndURL(responseJson))
+    .catch(error => alert('Something went wrong try again later'))
+
+}
+
+function getRepoNameAndURL(responseJson) {
+  $('.results').empty();
+  var searchResults = responseJson.map(function (key) {
+
+    return (`<div>URL :<a href="${key.url}">${key.url}</a></div>
+    <div> name : ${key.name}</div>`);
+  });
+  if (responseJson.message == "(Not Found)") {
+    
+    $(".result").append("user not found");
+  } 
+  
+  else {
+    $(".results").html(searchResults);
+    $(".result").removeClass("hidden");
+  }
+}
